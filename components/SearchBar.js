cb6b323da63c8c36a173be0cf8aedd632b193b83
shubh baghel <78901584+shubhdo42@users.noreply.github.com>
2021-10-09 12:08:14 +0530

    intial commit

---
+++ components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Fontisto from "react-native-vector-icons/Entypo";
+
+function SearchBar() {
+  return (
+    <View style={styles.container}>
+      <Text>SearchBar</Text>
+    </View>
+  );
+}
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  container: {},
+});
---
+++ screens/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, SafeAreaView, StyleSheet } from "react-native";
+import ContactsMenu from "../components/ContactsMenu";
+import Header from "../components/Header";
+import MenuButtons from "../components/MenuButtons";
+import SearchBar from "../components/SearchBar";
+function Home() {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={{ height: "100%" }}>
+        <Header />
+        {/* Header */}
+        {/* Searchbar */}
+        <SearchBar />
+        {/* Menu Buttons */}
+        <MenuButtons />
+        {/* {Contacts} */}
+        <ContactsMenu />
+      </SafeAreaView>
+    </View>
+  );
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#1c1c1c",
+    padding: 15,
+    height: "100%",
+  },
+});
